a8438a7d9f37ee8ff91a1ae9c10af57a975a9e8c
Kovtunenko Anton <kovtunenko-anton@mail.ru>
2022-02-03 18:07:55 +0300

    Update symfonyMailer.sh
    don't  use -ti for cron

--- symfonyMailer.sh
+++ symfonyMailer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/bin/docker exec -ti php-fpm bash -c "php bin/console mail:collect && php bin/console swiftmailer:spool:send"
+/bin/docker exec php-fpm bash -c "php bin/console mail:collect && php bin/console swiftmailer:spool:send"
